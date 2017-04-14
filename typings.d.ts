2ea732c44bd774b295c30ae74bcd4f5780134feb
Rob Grindeland <grind086@users.noreply.github.com>
2017-04-14 03:10:24 -0500

    fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -4,17 +4,21 @@ declare module 'g-log' {
         transports?: Transport[];
     }
     
+    interface IMeta {
+        [key: string]: any
+    }
+    
     export class Logger {
         level: number;
         name: string;
-        meta: object;
+        meta: IMeta;
         transports: Transport[];
         
-        constructor(level: number | string, meta?: object, config?: ILoggerConfig);
-        mergeMeta(...metas: object[]): object;
-        log(level: string, msg: string, meta: object): void;
-        child(level: number, meta?: object, config?: ILoggerConfig): Logger;
-        child(meta?: object, config?: ILoggerConfig): Logger;
+        constructor(level: number | string, meta?: IMeta, config?: ILoggerConfig);
+        mergeMeta(...metas: IMeta[]): IMeta;
+        log(level: string, msg: string, meta: IMeta): void;
+        child(level: number, meta?: IMeta, config?: ILoggerConfig): Logger;
+        child(meta?: IMeta, config?: ILoggerConfig): Logger;
         remove(): void;
         
         static levels: { [level: string]: number };
@@ -23,12 +27,12 @@ declare module 'g-log' {
         static nextid(): number;
         
         // Default log levels
-        error(msg: string | Error, meta?: object): void;
-        warn(msg: string | Error, meta?: object): void;
-        info(msg: string | Error, meta?: object): void;
-        verbose(msg: string | Error, meta?: object): void;
-        debug(msg: string | Error, meta?: object): void;
-        silly(msg: string | Error, meta?: object): void;
+        error(msg: string | Error, meta?: IMeta): void;
+        warn(msg: string | Error, meta?: IMeta): void;
+        info(msg: string | Error, meta?: IMeta): void;
+        verbose(msg: string | Error, meta?: IMeta): void;
+        debug(msg: string | Error, meta?: IMeta): void;
+        silly(msg: string | Error, meta?: IMeta): void;
     }
     
     export class Transport {
@@ -37,31 +41,31 @@ declare module 'g-log' {
         level: number;
         
         constructor(level?: number | string, ...args: any[]);
-        private _log(str: string, data: object): void;
-        log(str: string, data: object): void;
+        private _log(str: string, data: IMeta): void;
+        log(str: string, data: IMeta): void;
     }
     
     export var version: string;
     export var Transports: { [name: string]: { new(...args: any[]): Transport }};
     export var levels: { [level: string]: number };
-    export function create(level: number, meta?: object, config?: ILoggerConfig): Logger;
+    export function create(level: number, meta?: IMeta, config?: ILoggerConfig): Logger;
     export function exists(name: string): boolean;
-    export function get(name: string, defaultLevel?: string | number, meta?: object): Logger;
+    export function get(name: string, defaultLevel?: string | number, meta?: IMeta): Logger;
     export function remove(name: string): void;
     
     // From Logger class
     export var level: number;
     export var name: string;
-    export var meta: object;
+    export var meta: IMeta;
     export var transports: Transport[];
-    export function mergeMeta(...metas: object[]): object;
-    export function log(level: string, msg: string, meta: object): void;
-    export function child(level: number | string, meta?: object, config?: ILoggerConfig): Logger;
-    export function child(meta?: object, config?: ILoggerConfig): Logger;
-    export function error(msg: string | Error, meta?: object): void;
-    export function warn(msg: string | Error, meta?: object): void;
-    export function info(msg: string | Error, meta?: object): void;
-    export function verbose(msg: string | Error, meta?: object): void;
-    export function debug(msg: string | Error, meta?: object): void;
-    export function silly(msg: string | Error, meta?: object): void;
+    export function mergeMeta(...metas: IMeta[]): IMeta;
+    export function log(level: string, msg: string, meta: IMeta): void;
+    export function child(level: number | string, meta?: IMeta, config?: ILoggerConfig): Logger;
+    export function child(meta?: IMeta, config?: ILoggerConfig): Logger;
+    export function error(msg: string | Error, meta?: IMeta): void;
+    export function warn(msg: string | Error, meta?: IMeta): void;
+    export function info(msg: string | Error, meta?: IMeta): void;
+    export function verbose(msg: string | Error, meta?: IMeta): void;
+    export function debug(msg: string | Error, meta?: IMeta): void;
+    export function silly(msg: string | Error, meta?: IMeta): void;
 }
